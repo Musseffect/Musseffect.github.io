49b7a590d6181d2c05e5d86d72936db170ad00dc
Musseffect <lazarevich356@gmail.com>
2020-11-24 21:46:31 +0700

    copy to clipboard function

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,17 @@ export function dateToString(date)
     const minmin = String(date.getMinutes()).padStart(2, '0');
     return `${dd}-${mm}-${yyyy} ${hh}:${minmin}`;
 }
+
+
+export const copyToClipBoard = (str) =>
+{
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
