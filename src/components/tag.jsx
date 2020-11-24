83feb5a122dcd9512e68fa5f5d407a4845881014
Musseffect <lazarevich356@gmail.com>
2020-11-24 21:49:11 +0700

    unified tag system

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -6,11 +6,16 @@ class Tag extends Component {
   {
       super(props);
       this.click=props.click;
+      this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e){
+    if(this.props.click)
+      this.props.click(this.props.name);
   }
   render() {
-    let {active,name,click} = this.props;
+    let {active,name} = this.props;
     return (
-      <div className={'tag'+(active?' active':'')} onClick={click}>
+      <div className={'tag'+(active?' active':'')} onClick={this.handleClick}>
         <div className="tagCircle"></div>
         <div className="tagName">{name}</div>
       </div>
